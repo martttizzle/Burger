4afda336c2382463ff3fd6b0c3f0bd7abc1df79c
Martel Ramdin <martel1983@yahoo.com>
2018-06-20 10:55:45 -0500

    second setup

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Big Mac', true);
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Whopper', true);
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Baconator', true);
+ 
